d89fb99718ed8d1513c35e2e1ce02ca7790407ce
Dany Cervantes <dcervantes95.16@gmail.com>
2020-10-25 14:59:06 -0600

    Slider tipo Netflix funcionando

--- sliderProducts.js
+++ sliderProducts.js
@@ -4,33 +4,33 @@ const right = document.querySelector(".iconRight")
 console.log(right)
 left.addEventListener('click',scrollLeft)
 right.addEventListener('click',scrollRight)
-const slidesTotal = 3 //[0,1,2,3]
+const slidesTotal = 2 //[0,1,2,3]
 let slideNumber = 0
 function scrollLeft(){
     if (slideNumber==0) {
         slideNumber = 0
         let slidePercent = -slideNumber*100 + '%'
-        document.getElementById("list").style.marginLeft = slidePercent
+        document.getElementById("carrusel__container").style.marginLeft = slidePercent
         console.log(slidePercent)
     }
     else {
         slideNumber --
         let slidePercent = -slideNumber*100 + '%'
-        document.getElementById("list").style.marginLeft =slidePercent  
+        document.getElementById("carrusel__container").style.marginLeft =slidePercent  
         console.log(slidePercent)
     }
     console.log(slideNumber)
 }
 function scrollRight(){
     if (slideNumber==slidesTotal) {
-        slideNumber = 3 //[0,1,2,3]
+        slideNumber = slidesTotal //[0,1,2,3]
         let slidePercent = -slideNumber*100 + '%'
-        document.getElementById("list").style.marginLeft = slidePercent
+        document.getElementById("carrusel__container").style.marginLeft = slidePercent
         console.log(slidePercent)
     } else {
         slideNumber ++
         let slidePercent = -slideNumber*100 + '%'
-        document.getElementById("list").style.marginLeft = slidePercent   
+        document.getElementById("carrusel__container").style.marginLeft = slidePercent   
         console.log(slidePercent)
     }
     console.log(slideNumber)
